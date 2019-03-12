71ee71976c65e19d263ea5c1c7f6d6fb0604a6c0
JacobDozier <jacobadozier@gmail.com>
2019-03-12 11:29:46 -0500

    Added helper functions to call the Zendesk API to get the necessary info. Also cleaned up some things.

--- assets/main.js
+++ assets/main.js
@@ -1,14 +1,37 @@
 $(function() {
     var client = ZAFClient.init();
     client.invoke('resize', {width: '100%', height: '120px'});
+    client.get('ticket.requester.id').then(
+        function(data) {
+            var user_id = data['ticket.requester.id'];
+            requestUserInfo(client, user_id);
+        }
+    );
 });
 
-function showInfo() {
+function requestUserInfo(client, id) {
+    var settings = {
+        url: '/api/v2/users/' + id + '.json',
+        type: 'GET',
+        dataType: 'json'
+    };
+
+    client.request(settings).then(
+        function(data) {
+            showInfo(data);
+        },
+        function(response) {
+            showError(response);
+        }
+    );
+}
+
+function showInfo(data) {
     var requester_data = {
-        'name': 'Jane Doe',
-        'tags': ['tag1', 'tag2'],
-        'created_at': 'November 20, 2014',
-        'last_login_at': 'June 27, 2016'
+        'name': data.user.name,
+        'tags': data.user.tags,
+        'created_at': formatDate(data.user.created_at),
+        'last_login_at': formatDate(data.user.last_login_at)
     };
 
     var source = $("#requester-template").html();
@@ -17,13 +40,25 @@ function showInfo() {
     $("#content").html(html);
 }
 
-function showError() {
+function showError(response) {
     var error_data = {
-      'status': 404,
-      'statusText': 'Not found'
+      'status': response.status,
+      'statusText': response.statusText
     };
     var source = $("#error-template").html();
     var template = Handlebars.compile(source);
     var html = template(error_data);
     $("#content").html(html);
+}
+
+
+function formatDate(date) {
+    var cdate = new Date(date);
+    var options = {
+        year: "numeric",
+        month: "short",
+        day: "numeric"
+    };
+    date = cdate.toLocaleDateString("en-us", options);
+    return date;
 }
